def0003473fdc696b991487cc11e571ec7f80fd6
matheusferro <matheussouzaferro@gmail.com>
2021-03-08 19:10:58 -0300

    map - criacao e leitura

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,4 +2,18 @@ import java.text.CollationElementIterator
 
 fun main(args: Array<String>) {
 
+    //Criando um Map
+    var pedidos: Map<Int, Double> = mapOf<Int, Double>(Pair(1, 20.0), Pair(2, 45.0), 3 to 50.0)
+    println(pedidos)
+
+    //Leitura
+    var pedido: Double? = pedidos[0]
+    pedido?.let {
+        print("Pedido $it")
+    }
+
+    for (pedido: Map.Entry<Int, Double> in pedidos){
+        println("Número do pedido: ${pedido.key}")
+        println("Valor do pedido: ${pedido.value}")
+    }
 }
