0c20938bc90d76623275ef12695db65388766c16
matheusferro <matheussouzaferro@gmail.com>
2021-03-08 18:50:24 -0300

    Set e mutableSet

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,19 @@
 import java.text.CollationElementIterator
 
 fun main(args: Array<String>) {
-    var assistiramCursoAndroid: List<String> = listOf("Matheus","Jorge","Pedro","João")
+    //utilizando o elemento set, que é uma lista de elementos unicos
+    var assistiramCursoAndroid: Set<String> = setOf("Matheus","Jorge","Pedro","João")
     var assistiramCursoKotlin: List<String> = listOf("Matheus","Vitor","Paulo","João")
-    var assistiramDoisCursos: List<String> = assistiramCursoAndroid + assistiramCursoKotlin
-    println(assistiramDoisCursos.distinct())
+    //Aqui a ordem importa, precisamos receber um Set. (set) = (set) + (list)
+    var assistiramDoisCursos: Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
+    println(assistiramDoisCursos)
+
+    var setMutavelAndroid: MutableSet<String> = mutableSetOf("Matheus","Jorge","Pedro","João")
+    var setMutavelKotlin: MutableSet<String> = mutableSetOf("Matheus","Vitor","Paulo","João")
+    setMutavelAndroid.add("Matheus") //Não vai ser adicionado, justamente por já existir o elemento "Matheus"
+    setMutavelAndroid.add("Marcio") //Vai ser adicionado
+    var doisSets: MutableSet<String> = mutableSetOf<String>()
+    doisSets.addAll(setMutavelAndroid)
+    doisSets.addAll(setMutavelKotlin)
+    println(doisSets)
 }
