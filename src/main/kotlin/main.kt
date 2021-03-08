0e49d3cb8be617254d18efc44353a637962d0d85
matheusferro <matheussouzaferro@gmail.com>
2021-03-08 19:28:43 -0300

    Operacoes com map

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,16 +3,41 @@ import java.text.CollationElementIterator
 fun main(args: Array<String>) {
 
     //Criando um Map
-    var pedidos: MutableMap<Int, Double> = mutableMapOf<Int, Double>(Pair(1, 20.0), Pair(2, 45.0), 3 to 50.0)
+    var pedidos: MutableMap<Int, Double> = mutableMapOf<Int, Double>(
+        Pair(1, 20.0),
+        Pair(2, 45.0),
+        3 to 50.0,
+        4 to 68.0
+    )
     println(pedidos)
 
+    //operacoes especificas de Map
+    var pedido: Double? = pedidos.getValue(4) //Traz o valor null. Porém, se o index não existir lança exception.
+    println(pedido)
+
+    println(pedidos.getOrElse(5,{ "Sem pedido"}))
+
+    println(pedidos.filter { (chave, valor) ->
+        valor % 2 == 0.0
+    })
+
+    println(pedidos.filterKeys { chave ->
+        chave % 2 == 0
+    })
+
+    println(pedidos.filterValues { valor ->
+        valor > 40
+    })
+}
+
+fun testaMap(pedidos: MutableMap<Int, Double>) {
     //Leitura
     var pedido: Double? = pedidos[0]
     pedido?.let {
         print("Pedido $it")
     }
 
-    for (p: Map.Entry<Int, Double> in pedidos){
+    for (p: Map.Entry<Int, Double> in pedidos) {
         println("Número do pedido: ${p.key}")
         println("Valor do pedido: ${p.value}")
     }
