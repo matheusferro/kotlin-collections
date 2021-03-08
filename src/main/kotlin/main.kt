5e42d56dd0ab3558d144a3f6e3bb4f776bd9bb60
matheusferro <matheussouzaferro@gmail.com>
2021-03-08 18:26:53 -0300

    Tipos de colecao

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,35 @@
 fun main(args: Array<String>) {
+    val banco = BancoDeNomes()
+    banco.salva("Thais")
+    println(banco.nomes)
+}
 
-    val listaImutavel: Collection<String> = listOf(
+class BancoDeNomes{
+
+    /**
+     * Externaliza uma variavel "copia"
+     * do objeto que nos manipulamos internamente.
+     */
+    val nomes: Collection<String> get() = dadosInterno
+
+    fun salva(nome: String){
+        dadosInterno.add(nome)
+    }
+
+    companion object{
+        val dadosInterno = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
+    val listaImutavel: Collection<String> = mutableListOf(
         "Alex",
         "Fran",
         "Gui",
         "Maria",
         "Ana"
     )
-    for (nome in listaImutavel){
+    for (nome in listaImutavel) {
         println(nome)
     }
     println(listaImutavel)
@@ -16,5 +38,4 @@ fun main(args: Array<String>) {
      * Comportamento .size nao é compativel com Iterable<>
      */
     println("Tamanho da coleção ${listaImutavel.size}")
-
 }
