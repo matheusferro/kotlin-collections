60a446616c6bd3eb6bc331cce6ac4e491dfe86f1
matheusferro <matheussouzaferro@gmail.com>
2021-03-08 18:17:00 -0300

    Introducao a collections

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,20 @@
 fun main(args: Array<String>) {
 
+    val listaImutavel: Collection<String> = listOf(
+        "Alex",
+        "Fran",
+        "Gui",
+        "Maria",
+        "Ana"
+    )
+    for (nome in listaImutavel){
+        println(nome)
+    }
+    println(listaImutavel)
+    println("Tem o nome Alex? ${listaImutavel.contains("Alex")}")
+    /**
+     * Comportamento .size nao é compativel com Iterable<>
+     */
+    println("Tamanho da coleção ${listaImutavel.size}")
+
 }
