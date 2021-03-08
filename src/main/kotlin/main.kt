6b15ac22488812b0a90b3769f54f045a7d8df072
matheusferro <matheussouzaferro@gmail.com>
2021-03-08 18:38:34 -0300

    Usando o .distinct()

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,51 +1,8 @@
 import java.text.CollationElementIterator
 
 fun main(args: Array<String>) {
-    val banco = BancoDeNomes()
-    banco.salva("Thais")
-    /**
-     * Só possivel utilizar Collection
-     */
-    val listaMutavelCopia: Collection<String> = banco.nomes
-    println(listaMutavelCopia)
-    println(banco.nomes)
-}
-
-class BancoDeNomes{
-
-    /**
-     * Externaliza uma variavel "copia"
-     * do objeto que nos manipulamos internamente.
-     *
-     * Adicionando o .toList() retorna uma cópia da lista interna
-     * impossibilitando o casting.
-     */
-    val nomes: Collection<String> get() = dadosInterno.toList()
-
-    fun salva(nome: String){
-        dadosInterno.add(nome)
-    }
-
-    companion object{
-        val dadosInterno = mutableListOf<String>()
-    }
-}
-
-fun testaColecao() {
-    val listaImutavel: Collection<String> = mutableListOf(
-        "Alex",
-        "Fran",
-        "Gui",
-        "Maria",
-        "Ana"
-    )
-    for (nome in listaImutavel) {
-        println(nome)
-    }
-    println(listaImutavel)
-    println("Tem o nome Alex? ${listaImutavel.contains("Alex")}")
-    /**
-     * Comportamento .size nao é compativel com Iterable<>
-     */
-    println("Tamanho da coleção ${listaImutavel.size}")
+    var assistiramCursoAndroid: List<String> = listOf("Matheus","Jorge","Pedro","João")
+    var assistiramCursoKotlin: List<String> = listOf("Matheus","Vitor","Paulo","João")
+    var assistiramDoisCursos: List<String> = assistiramCursoAndroid + assistiramCursoKotlin
+    println(assistiramDoisCursos.distinct())
 }
